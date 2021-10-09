67c124dac9e1bac1a99a6ac77161e54c9ce5b7ad
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-08 21:17:59 -0300

    Implementada estructura switch

--- src/EstruturaSwitch.java
+++ src/EstruturaSwitch.java
@@ -3,18 +3,36 @@ import java.util.Scanner;
 public class EstruturaSwitch {
 
 	public static void main(String[] args) {
-		/*Crie um programa que permita ao usuário escolher o elogio que gostaria de receber, de acordo com a seguinte regra: 1 - Elogio profissional, 2 - Elogio físico, 3 - Elogio pessoal
-		 * Caso digite qualquer outro número, deve ser exibida a mensagem "opção inválida"*/
+		/*
+		 * Crie um programa que permita ao usuário escolher o elogio que gostaria de
+		 * receber, de acordo com a seguinte regra: 1 - Elogio profissional, 2 - Elogio
+		 * físico, 3 - Elogio pessoal Caso digite qualquer outro número, deve ser
+		 * exibida a mensagem "opção inválida"
+		 */
 		Scanner leitor = new Scanner(System.in);
 		int opcao;
 		System.out.println("ELOGIADOR!");
 		System.out.println("Selecione a opção desejada:\n1-Elogio profissional\n2-Elogio físico\n3-Elogio pessoal");
 		opcao = leitor.nextInt();
-	
-		//Aqui precisamos de uma estrutura para validar a opção inserida.
+
+		// Aqui precisamos de uma estrutura para validar a opção inserida.
+		// Break para o funcionamento do case e sai da estrutura
+
+		switch (opcao) {
+		case 1:
+			System.out.println("Os seus commit são lindos!");
+			break;
+		case 2:
+			System.out.println("O seu nariz aspira bastante ar");
+			break;
+		case 3:
+			System.out.println("Você é uma pessoa maravilhosa");
+			break;
+		default:
+			System.out.println("Opção inválida");
+			break;
+		}
 
 		leitor.close();
-
 	}
-
 }
