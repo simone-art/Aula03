bf2c995410280f6b3fba18865405dd1d2b798153
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-08 21:02:58 -0300

    Implementado o ! para invertir valores

--- src/OperadorNao.java
+++ src/OperadorNao.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 public class OperadorNao {
 
 	public static void main(String[] args) {
-		/*Crie um programa que permita que receba o tipo de um usuário em uma variável String e caso esse tipo NÃO SEJA "administrador", exibir a mensagem: "Atenção, usuário sem poderes administrativos. Algumas tarefas serão bloqueadas".*/
+		/*Crie um programa que permita que receba o tipo de um usuário em uma variável String 
+		 * e caso esse tipo NÃO SEJA "administrador", 
+		 * exibir a mensagem: "Atenção, usuário sem poderes administrativos. Algumas tarefas serão bloqueadas".*/
 		Scanner leitor = new Scanner(System.in);
 		String tipoUsuario;
 		System.out.println("Por favor, digite seu tipo de usuário:");
 		tipoUsuario = leitor.next();
 		
 		//Aqui é preciso criar um if que verifique se o tipo de usuário digitado é DIFERENTE de "administrador"
+		//Operador ! que inverte o estado lógico. Se for falso, vai ser verdadeiro e viceversa. 
+		if(!tipoUsuario.equals("administrador")) {
+			System.out.println("Usuário sem poderes administrativos");
+		}
 		
 		leitor.close();
-		
-
 	}
 
 }
